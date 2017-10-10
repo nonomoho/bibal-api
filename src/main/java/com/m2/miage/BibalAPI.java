21747d081971ca746058bb07e64e3b5713f8eabe
Thomas Schersach <toj315@gmail.com>
2017-10-10 07:32:19 +0200

    Fix app name

--- src/main/java/com/m2/miage/BibalAPI.java
+++ src/main/java/com/m2/miage/BibalAPI.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BibalServciceApplication {
+public class BibalAPI {
 
   public static void main(String[] args) {
-    SpringApplication.run(BibalServciceApplication.class, args);
+    SpringApplication.run(BibalAPI.class, args);
   }
 }
--- src/test/java/com/m2/miage/BibalAPITests.java
+++ src/test/java/com/m2/miage/BibalAPITests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BibalServciceApplicationTests {
+public class BibalAPITests {
 
 	@Test
 	public void contextLoads() {
